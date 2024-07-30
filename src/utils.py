c21132b9417fe9a00407c8bf21776760b96a6c7c
Alexandr Labutin <mazhorrr00@gmail.com>
2024-07-30 22:39:45 +0500

    Delete requirements.txt, fix for lint

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import json
 import os
+from logging_config import setup_logger
 from typing import Any, Dict, List
 
 
@@ -11,21 +12,19 @@ def load_transactions(path: str) -> List[Dict[str, Any]]:
                             path = 'path/transactions.json'
                             print(load_transactions(path))
     """
+    logger = setup_logger("utils", "utils.log")
     if not os.path.exists(path):
+        logger.info("OK")
         return []
-
     try:
+        logger.info("OK")
         with open(path, 'r', encoding='utf-8') as file:
             data = json.load(file)
             if isinstance(data, list):
                 return data
             else:
                 return []
-    except (json.JSONDecodeError, FileNotFoundError):
+
+    except (json.JSONDecodeError, FileNotFoundError) as e:
+        logger.error(f"Error: {e}")
         return []
-
-
-# Пример использования функции:
-'''file_path = 'path/to/your/transactions.json'
-transactions = load_transactions(path)
-print(transactions)'''
--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from logging_config import setup_logger
 
 
 def mask_card(numbers_card_check: str) -> str:
@@ -16,40 +17,49 @@ def mask_card(numbers_card_check: str) -> str:
         Visa Gold 5999414228426353
         Счет 73654108430135874305
     """
+    logger = setup_logger('masks', 'masks.log')
+    try:
+        if len(numbers_card_check.split()[-1]) == 16:
+            rep_symbol = ""
+            mask = ""
+            count = 0
+            start = " ".join(
+                numbers_card_check.split()[:-1]
+            )  # Объединяем список обратно в строку
+            number = numbers_card_check.split()[-1]
 
-    if len(numbers_card_check.split()[-1]) == 16:
-        start = ""
-        rep_symbol = ""
-        mask = ""
-        count = 0
-        start = " ".join(
-            numbers_card_check.split()[:-1]
-        )  # Объединяем список обратно в строку
-        number = numbers_card_check.split()[-1]
+            for i in number:
+                count += 1
+                if 7 <= count <= 12:
+                    i = "*"
+                rep_symbol += i
 
-        for i in number:
-            count += 1
-            if 7 <= count <= 12:
-                i = "*"
-            rep_symbol += i
+            count = 0
+            for i in rep_symbol:
+                count += 1
+                if count < 4:
+                    mask += i
+                else:
+                    mask += i
+                    mask += " "
+                    count = 0
+            mask = mask.rstrip()
+            logger.info("ОК")
 
-        count = 0
-        for i in rep_symbol:
-            count += 1
-            if count < 4:
-                mask += i
-            else:
-                mask += i
-                mask += " "
-                count = 0
-        mask = mask.rstrip()
-        return f"{start} {mask}"
-    else:
-        if len(numbers_card_check.split()[-1]) == 20:
-            start = " ".join(numbers_card_check.split()[:-1])
-            return f"{start} **{numbers_card_check[-4:]}"
+            return f"{start} {mask}"
         else:
-            return "Введён некорректный номер счёта или карты!"
+            if len(numbers_card_check.split()[-1]) == 20:
+                start = " ".join(numbers_card_check.split()[:-1])
+                logger.info("ОК")
+                return f"{start} **{numbers_card_check[-4:]}"
+            else:
+                logger.warning("Введён некорректный номер счёта или карты!")
+                return "Введён некорректный номер счёта или карты!"
+
+
+
+    except Exception as e:
+        logger.error(f"Error: {e}")
 
 
 def date(curr_date: str) -> str:
@@ -59,3 +69,6 @@ def date(curr_date: str) -> str:
     date_input = datetime.strptime(curr_date, "%Y-%m-%dT%H:%M:%S.%f")
     date_output = datetime.strftime(date_input, "%d.%m.%y")
     return date_output
+
+
+print(mask_card("Maestro 7863711"))
